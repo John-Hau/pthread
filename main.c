c9e36d773459c21a11fd90621e6a4e9021b821dc
U-GLOBAL\H374389 <H374389@CH5OLT23420Z2.global.ds.honeywell.com>
2021-05-12 14:29:57 +0800

    add com_uart_port.c and com_uart_port.h

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
-
+#include <mqueue.h>
 #define TEST_CNT    100
+
+#define MSG_IPC_BUF_SIZE  8192
+
 //gcc -D_REENTRANT -D_POSIX_C_SOURCE  thread2.c -o thread2.exe   -lpthread
 
 
@@ -13,42 +18,99 @@ extern FILE*fp;
 
 pthread_mutex_t  mutex_file = PTHREAD_MUTEX_INITIALIZER;
 
+#define MSG_LEN  128
+typedef mqd_t msg_id_t;
+
+typedef struct msg_ipc
+{
+	uint8_t len;
+	uint8_t msg_content[MSG_LEN];
+}msg_ipc_t;
+
+
+mqd_t mqfd;
 
 
 
+uint8_t msg_sbuffer[8192]={0};
+uint8_t msg_rbuffer[8192]={0};
 
-
+struct mq_attr attr;
 
 void*thread_function(void*arg);
 void*thread_read(void*arg);
 
 char message[64] = "Hello World";
 
-
-
 void *thread_function(void*arg)
 {
-
 	printf("thread function is running. Argument was %s\n",(char*)arg);
-
 	sleep(3);
 	strcpy(message,"Bye");
-
 	pthread_exit("Thank you for the CPU time\n");
-
 }
+
+
+
 
 void *thread_read(void*arg)
 {
 
 	printf("thread read is running. Argument was %s\n",(char*)arg);
-
 	sleep(3);
 	strcpy(message,"Bye");
-
 	pthread_exit("Thank you for the CPU time\n");
 
 }
+
+
+
+
+int mq_msg_get(msg_id_t msg_id,msg_ipc_t *msg)
+{
+
+
+	int nr=mq_receive(mqfd,msg_rbuffer,8192,0);
+	return nr;
+}
+
+
+int mq_msg_put(msg_id_t msg_id,msg_ipc_t *msg)
+{
+
+	int ns=mq_send(mqfd,msg->msg_content,msg->len,0);
+	return ns;
+}
+
+
+
+void *thread_get_message_from_uart_port(void*arg)
+{
+
+	uint32_t cnt=0;
+
+	char c='1';
+	uint32_t m=0;
+	while(m<10)
+	{
+		pthread_mutex_lock(&mutex_file);
+		//for(cnt=0;cnt<TEST_CNT;cnt++)
+		for(cnt=0;cnt<9;cnt++)
+		{
+			//printf("pthread_write_console %d\n",cnt+1);
+			//printf("abcdefghijklmnopqrstuvwxyz",cnt+1);
+			fputc(c+cnt,fp);
+		}
+
+		pthread_mutex_unlock(&mutex_file);
+		m++;
+	}
+	pthread_exit("pthread_write_console,Thank you for the CPU time\n");
+
+}
+
+
+
 
 
 void *thread_write_uart(void*arg)
@@ -58,7 +120,7 @@ void *thread_write_uart(void*arg)
 	char c='a';
 	uint32_t m=0;
 
-	while(m < 100)
+	while(m < 10)
 	{
 		pthread_mutex_lock(&mutex_file);
 		//for(cnt=0;cnt<TEST_CNT;cnt++)
@@ -78,6 +140,7 @@ void *thread_write_uart(void*arg)
 }
 
 
+
 void *thread_write_console(void*arg)
 {
 
@@ -85,7 +148,7 @@ void *thread_write_console(void*arg)
 
 	char c='1';
 	uint32_t m=0;
-	while(m<100)
+	while(m<10)
 	{
 		pthread_mutex_lock(&mutex_file);
 		//for(cnt=0;cnt<TEST_CNT;cnt++)
@@ -121,6 +184,34 @@ int main(int argc ,char* argv[])
 	void* thread_read_result;
 	void* thread_write_uart_result;
 	void* thread_write_console_result;
+
+	attr.mq_maxmsg=16;
+	attr.mq_msgsize=MSG_IPC_BUF_SIZE;
+	attr.mq_flags=0;
+
+	//mode_t mode  = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+	mqfd=mq_open("myipc",O_RDWR|O_CREAT,0644,&attr);
+
+	if(mqfd == -1)
+	{
+		perror("mq_open failure");
+		exit(EXIT_FAILURE);
+	}
+
+	strcpy(msg_sbuffer,"hello");
+	int ns=mq_send(mqfd,msg_sbuffer,8,0);
+	int nr=mq_receive(mqfd,msg_rbuffer,MSG_IPC_BUF_SIZE,0);
+
+	if(nr >0)
+	{
+		printf("receiving OK nr is %d msg_rbuffer is %s\n",nr,msg_rbuffer);
+	}
+
+	mq_close(mqfd);
+	//mq_unlink("./myipc");
+	printf("end\n");
+	return 0;
+
 
 	fp = fopen("test.log","wt");
 	if(fp == NULL)
